1cd5a916edb4bbbbcc9c44bf47198d0df01a1d07
iuliavoit <you@example.comiuliavoit13@yahoo.com>
2022-05-08 16:26:55 +0300

    c2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {ButtonModule} from "primeng/button";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    ButtonModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
