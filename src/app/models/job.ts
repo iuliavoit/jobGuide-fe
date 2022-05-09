a3fbe6dc246dd11a74104c1084a04710a1600990
iuliavoit <you@example.comiuliavoit13@yahoo.com>
2022-05-10 00:13:20 +0300

    some datamodels

---
+++ src/app/models/job.ts
@@ -0,0 +1,7 @@
+export interface Job {
+  type: string,
+  date: Date,
+  employerName: string,
+  description: string,
+  nrOfCandidates: number
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number,
+  role: string,
+  firstName: string,
+  lastName: string,
+  age: number,
+  email: string,
+  password: string
+}
